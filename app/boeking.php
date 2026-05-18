d952ea0ae30e3a3f5331e637452b583f4977525b
Mitsubi <agp.842007@gmail.com>
2026-05-18 16:51:48 +0200

    footer, background, menu and comments
    i added a footer
    i added background at the start with name of the page
    i changed some menu ui
    i also placed some comments where everything will be placed.

--- app/boeking.php
+++ app/boeking.php
@@ -1,29 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>booking</title>  
-    <link rel="stylesheet" href="reisbureau.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>booking</title>
+  <link rel="stylesheet" href="reisbureau.css">
 </head>
-  <body>
 
-    <nav class="navbar">
+<body>
+  <!--navigation menu -->
+  <nav class="navbar">
+    <div class="logo">
+      <div class="logo-icon"></div>
+      <h2>TungSahara</h2>
+    </div>
+    <div class="nav-menu">
+      <a href="index.php">Home</a>
+      <a href="informatie.php">Information</a>
+      <a href="boeking.php" class="active">Booking</a>
+      <a href="login.php" class="login-btn">Login</a>
+    </div>
+  </nav>
+  <!--achtergrond -->
+  <div class="background">
+    <h1>test</h1>
+  </div>
+  <!--reserveringsformulier -->
+  <!--kleine informatie balkjes -->
+  <!--footer -->
+</body>
 
-      <div class="logo">
-        <div class="logo-icon"></div>
-        <h2>TungSahara</h2>
-      </div>
-
-      <div class="nav-menu">
-        <a href="index.php">Home</a>
-        <a href="informatie.php">Information</a>
-        <a href="boeking.php" class="active">Booking</a>
-
-        <a href="login.php" class="login-btn active">Login</a>
-      </div>
-
-    </nav>
-
-  </body>
-  </html>
+</html>
--- app/login.php
+++ app/login.php
@@ -7,23 +7,20 @@
    <link rel="stylesheet" href="reisbureau.css">
 </head>
   <body>
-
+    <!--navigation menu -->
     <nav class="navbar">
-
       <div class="logo">
         <div class="logo-icon"></div>
         <h2>TungSahara</h2>
       </div>
-
       <div class="nav-menu">
         <a href="index.php">Home</a>
         <a href="informatie.php">Information</a>
         <a href="boeking.php">Booking</a>
-
         <a href="login.php" class="login-btn" class="active">Login</a>
       </div>
-
     </nav>
-
+<!--log in form -->
+<!--footer -->
   </body>
   </html>
